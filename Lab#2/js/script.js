9e46d30cb251fc2f5b8a3445a4484ac1806dbe3c
abelopejr1992 <abelopezjr1992@gmail.com>
2019-11-21 15:04:48 -0800

    11/21/2019

--- Lab#2/js/script.js
+++ Lab#2/js/script.js
@@ -4,11 +4,27 @@ let display = document.getElementById('display');
 let pCounter = 0;
 //Data Locations
 let todoList = [];
-let listItems = [];
+let obj = {
+    listItems: [
+        {
+            title: 'listTitle',
+            listContent: []
+        },
+        {
+            title: 'listTitle',
+            listContent: []
+        }
+    ]
+}
 
 //Test Arrays-----------------------------------------------------------------------------//
 let itemArr1 = [1, 2, 3, 4, 5, 6, 7, 8];
 let itemArr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//Variables
+let list = document.getElementById('list');
+let textArea = document.getElementById('textArea');
+let createBtn = document.getElementById('createBtn');
+let clearBtn = document.getElementById('clearBtn');
 //---------------,-----------------------Storage Example----------------------------------//
 //let names=['jon', 'joe', 'bob'];
 //localStorage.setItem('names', JSON.stringify(names));
@@ -20,78 +36,65 @@ let itemArr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // { title:'my first list',  items:['task1', 'task2', 'task3']}
 // { title:'my first list',  items:['task1', 'task2', 'task3']}
 //]
-//--------------------------------------Injections-----------------------------------------------//
 
-function loadHTML(url) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let myArr = this.responseText;
-            //Injected pages for List & Items
-            if (url == "../html/list.html") {
-                injectListPage(myArr);
-            }
-            else if (url == "../html/items.html") {
-                injectItemPage(myArr);
-            }
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
-}
+//---------------------------Lists & Item Creator-------------------------//
 
-//------------------------------------Injected Pages-------------------------------------//
-function injectListPage(info) {
-    display.innerHTML = info;
+//Variables
 
-    //Variables
-    let list = document.getElementById('list');
-    let keyPress = document.getElementById('keyPress');
-    let saveList = document.getElementById('saveList');
-    let clearList = document.getElementById('clearList');
+//-----------------------------Create list elements---------------------------//
 
-    //Storage Location for Injection
-    let storageItem = localStorage.getItem('Text');
 
-    if (localStorage.getItem('todo') != '') {
-        //console.log(JSON.parse(localStorage.getItem('todo')));
-        let todoLocal = JSON.parse(localStorage.getItem('todo'));
-        //populate array items into p tags.
-        for (let i = 0; i < todoLocal.length; i++) 
-        { }
-        //saving local storage array into our array
-        todoList = todoLocal;
+function populateListItems() {
+    console.log(todoList);
+    clearArea(list);
+    for (let i = 0; i < todoList.length; i++) {
+        //loop through list creating element
+        //append element to list
+        let li = document.createElement('li');
+        li.setAttribute('class', 'border');
+        li.innerText = todoList[i];
+        li.addEventListener('click', function (e) {
+
+        });
+
+        list.appendChild(li);
     }
-
-    saveList.addEventListener('click', function (e) {
-        //Creating Tags for List
-
-
-
-        //Local Storage can store up to 5MB needs a ('name', location.value)
-        localStorage.setItem('Text', keyPress.value);
-        //todoList.push(keyPress.value);
-
-        //Calling append
-
-        list.append(pElement);
-        keyPress.value = storageItem;
-
-        //console.log(pCounter);
-    });
-
-    //----------------------Clear List----------------//
-    clearList.addEventListener('click', function (e) {
-
-    });
-    //----------------------keyPress------------------//
-    keyPress.addEventListener('keyPress', function (e) {
-        if (e.code == 'Enter') {
-            createInput();
-        }
-    });
-
 }
+
+function clearArea(area) {
+    while (area.firstChild) {
+        area.removeChild(area.firstChild);
+    }
+}
+
+//------------------------------Storage Check-----------------------------------//
+if (localStorage.getItem('List') != '') {
+    if (localStorage.getItem('List')) {
+        todoList = JSON.parse(localStorage.getItem('List'));
+        populateListItems();
+    } else {
+        todoList = [];
+    }
+}
+
+createBtn.addEventListener('click', function (e) {
+    todoList.push(textArea.value);
+    populateListItems();
+    saveLocal();
+    //Local Storage can store up to 5MB needs a ('name', location.value)
+});
+
+//----------------------Clear List----------------//
+clearBtn.addEventListener('click', function (e) {
+    todoList = [];
+    populateListItems();
+    saveLocal();
+});
+
+function saveLocal() {
+    localStorage.setItem('List', JSON.stringify(todoList));
+}
+
 
 //Items Page
 function injectItemPage(info) {
@@ -99,25 +102,3 @@ function injectItemPage(info) {
     let list = document.getElementById('list');
     //Add button list
 }
-
-
-//Used in CreateInput & injectListPage
-let pElement = document.createElement('p');
-
-//Called when inputing List/Item
-function createInput(a) {
-    //Put data entered into list.
-    a = pElement.innerText;
-    pElement.innerText = keyPress.value;
-    pElement.setAttribute('class', 'list-group-item');
-    pElement.setAttribute('id', pCounter);
-    pElement.addEventListener('click', function (e) {
-        //Test
-        alert("I've been clicked");
-    })
-    list.append(pElement);
-    pCounter++;
-}
-
-//Initial Load
-loadHTML("../html/list.html")
