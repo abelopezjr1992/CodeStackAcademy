9981203ba2daaf3be4cdc5a1590756e81493f840
abelopejr1992 <abelopezjr1992@gmail.com>
2019-11-21 22:59:38 -0800

    11/21/2019

--- Lab#2/js/script.js
+++ Lab#2/js/script.js
@@ -4,18 +4,6 @@ let display = document.getElementById('display');
 let pCounter = 0;
 //Data Locations
 let todoList = [];
-let obj = {
-    listItems: [
-        {
-            title: 'listTitle',
-            listContent: []
-        },
-        {
-            title: 'listTitle',
-            listContent: []
-        }
-    ]
-}
 
 //Test Arrays-----------------------------------------------------------------------------//
 let itemArr1 = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -25,21 +13,7 @@ let list = document.getElementById('list');
 let textArea = document.getElementById('textArea');
 let createBtn = document.getElementById('createBtn');
 let clearBtn = document.getElementById('clearBtn');
-//---------------,-----------------------Storage Example----------------------------------//
-//let names=['jon', 'joe', 'bob'];
-//localStorage.setItem('names', JSON.stringify(names));
-//console.log(JSON.parse(localStorage.getItem('names')));
-//or
-//let localStorageItem=JSON.parse(localstorage.getItem('names')); --> console.log(localStorageItem);
-//let todoList=[ { title:'my first list',  items:['task1', 'task2', 'task3']}
-// { title:'my first list',  items:['task1', 'task2', 'task3']}
-// { title:'my first list',  items:['task1', 'task2', 'task3']}
-// { title:'my first list',  items:['task1', 'task2', 'task3']}
-//]
-
-//---------------------------Lists & Item Creator-------------------------//
-
-//Variables
+//let saveBtn = document.getElementById('saveBtn');
 
 //-----------------------------Create list elements---------------------------//
 
@@ -53,6 +27,7 @@ function populateListItems() {
         let li = document.createElement('li');
         li.setAttribute('class', 'border');
         li.innerText = todoList[i];
+        li.contentEditable = true;
         li.addEventListener('click', function (e) {
 
         });
@@ -72,15 +47,19 @@ if (localStorage.getItem('List') != '') {
     if (localStorage.getItem('List')) {
         todoList = JSON.parse(localStorage.getItem('List'));
         populateListItems();
-    } else {
+    }
+    else {
         todoList = [];
     }
 }
+
+
 
 createBtn.addEventListener('click', function (e) {
     todoList.push(textArea.value);
     populateListItems();
     saveLocal();
+    textArea.value = '';
     //Local Storage can store up to 5MB needs a ('name', location.value)
 });
 
@@ -90,6 +69,10 @@ clearBtn.addEventListener('click', function (e) {
     populateListItems();
     saveLocal();
 });
+
+//saveBtn.addEventListener('click', function (e) {
+
+//});
 
 function saveLocal() {
     localStorage.setItem('List', JSON.stringify(todoList));
